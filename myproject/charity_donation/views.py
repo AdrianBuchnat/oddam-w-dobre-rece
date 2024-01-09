d9c4012df0ebbeeec8ab8b326f42b89b84678a2f
Adrian Buchnat <adrianbuchnat@gmail.com>
2024-01-10 00:10:08 +0100

    Index template modify

--- myproject/charity_donation/views.py
+++ myproject/charity_donation/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Institution, Donation, Category
 
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, "charity_donation/index.html")
+
+        institutions = Institution.objects.all()
+        donations = Donation.objects.all()
+
+        organistaion_supported = set()
+        quantity_of_bags = 0
+        for donation in donations:
+            quantity_of_bags += donation.quantity
+            organistaion_supported.add(donation.user)
+
+        ctx = {
+            'instituions': institutions,
+            'quantity_of_bags': quantity_of_bags,
+            'organistaion_supported': len(organistaion_supported),
+        }
+
+        return render(request, "charity_donation/index.html", ctx)
 
 
 class MainFormView(View):
